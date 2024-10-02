b86307505915012bd4a684ac5ef223521bf7c91b
nktvit <nikita.vitkovsky@icloud.com>
2024-10-03 00:46:04 +0100

    updated url. Removed unused api code

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,38 +1,18 @@
-import { Component } from '@angular/core';
-import { MoviesGridComponent } from '../../components/movies-grid/movies-grid.component';
-import { SearchBoxComponent } from '../../components/search-box/search-box.component';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
+import {Component} from '@angular/core';
+import {MoviesGridComponent} from '../../components/movies-grid/movies-grid.component';
+import {SearchBoxComponent} from '../../components/search-box/search-box.component';
+import {NavbarComponent} from '../../components/navbar/navbar.component';
 import {JsonPipe, NgForOf} from "@angular/common";
 import {RouterLink} from "@angular/router";
-import { VercelApiService } from '../../services/vercel-api.service';
 
 @Component({
-    selector: 'app-main',
-    standalone: true,
-    templateUrl: './main.component.html',
-    styleUrl: './main.component.css',
+  selector: 'app-main',
+  standalone: true,
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
   imports: [SearchBoxComponent, MoviesGridComponent, NavbarComponent, NgForOf, RouterLink, JsonPipe]
 })
 export class MainComponent {
-  result: any;
-
-  constructor(private vercelApiService: VercelApiService) {}
-
-  getData() {
-    this.vercelApiService.getData().subscribe(
-      data => this.result = data,
-      error => console.error('Error:', error)
-    );
-  }
-
-  postData() {
-    const data = { message: 'Hello from Angular!' };
-    this.vercelApiService.postData(data).subscribe(
-      response => this.result = response,
-      error => console.error('Error:', error)
-    );
-  }
-
   featuredMovies = [
     {
       title: 'The Shawshank Redemption',
