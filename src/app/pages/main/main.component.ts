b2c49801cab8339ffda0fc7c46be6897aa449ae7
nktvit <nikita.vitkovsky@icloud.com>
2025-03-29 00:41:43 +0000

    Enhance main component layout and add trending/recent movie sections

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,20 +1,21 @@
-import {Component} from '@angular/core';
-import {SearchBoxComponent} from '../../components/search-box/search-box.component';
-import {NavbarComponent} from '../../components/navbar/navbar.component';
-import {MatMonthView} from "@angular/material/datepicker";
-import {PosterComponent} from "../../components/poster/poster.component";
-import {IMovie} from "../../interfaces/movie.interface";
+import {Component} from '@angular/core'
+import {SearchBoxComponent} from '../../components/search-box/search-box.component'
+import {NavbarComponent} from '../../components/navbar/navbar.component'
+import {MatMonthView} from "@angular/material/datepicker"
+import {PosterComponent} from "../../components/poster/poster.component"
+import {IMovie} from "../../interfaces/movie.interface"
+import {NgForOf} from "@angular/common"
 
 @Component({
   selector: 'app-main',
   standalone: true,
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
-  imports: [SearchBoxComponent, NavbarComponent, PosterComponent]
+  imports: [SearchBoxComponent, NavbarComponent, PosterComponent, NgForOf]
 })
 export class MainComponent {
 
-  featuredMovies : IMovie[] = [
+  featuredMovies: IMovie[] = [
     {
       Title: 'The Shawshank Redemption',
       Plot: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
@@ -33,7 +34,140 @@ export class MainComponent {
       Poster: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',
       imdbID: "tt0468569"
     },
+    {
+      Title: 'Parasite',
+      Plot: 'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
+      imdbID: 'tt6751668'
+    },
+  ]
+
+  trendingMovies: IMovie[] = [
+    {
+      Title: 'Inception',
+      Plot: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+      imdbID: 'tt1375666'
+    },
+    {
+      Title: 'Interstellar',
+      Plot: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+      imdbID: 'tt0816692'
+    },
+    {
+      Title: 'The Matrix',
+      Plot: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+      imdbID: 'tt0133093'
+    },
+    {
+      Title: 'Parasite',
+      Plot: 'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
+      imdbID: 'tt6751668'
+    },
+    {
+      Title: 'Joker',
+      Plot: 'In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg',
+      imdbID: 'tt7286456'
+    },
+    {
+      Title: 'The Avengers',
+      Plot: 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+      imdbID: 'tt0848228'
+    },
+    {
+      Title: 'Fight Club',
+      Plot: 'An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      imdbID: 'tt0137523'
+    },
+    {
+      Title: 'Pulp Fiction',
+      Plot: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      imdbID: 'tt0110912'
+    },
+    {
+      Title: 'The Lord of the Rings: The Fellowship of the Ring',
+      Plot: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+      imdbID: 'tt0120737'
+    },
+    {
+      Title: 'Whiplash',
+      Plot: 'A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student\'s potential.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BOTA5NDZlZGUtMjAxOS00YTRkLTkwYmMtYWQ0NWEwZDZiNjEzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+      imdbID: 'tt2582802'
+    }
   ];
 
-  protected readonly MatMonthView = MatMonthView;
+  recentMovies: IMovie[] = [
+    {
+      Title: 'Everything Everywhere All at Once',
+      Plot: 'An aging Chinese immigrant is swept up in an insane adventure, where she alone can save the world by exploring other universes connecting with the lives she could have led.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BYTdiOTIyZTQtNmQ1OS00NjZlLWIyMTgtYzk5Y2M3ZDVmMDk1XkEyXkFqcGdeQXVyMTAzMDg4NzU0._V1_SX300.jpg',
+      imdbID: 'tt6710474'
+    },
+    {
+      Title: 'The Whale',
+      Plot: 'A reclusive English teacher suffering from severe obesity attempts to reconnect with his estranged teenage daughter for one last chance at redemption.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BZDQ4Njg4YTctNGZkYi00NWU1LWI4OTYtNmNjOWMyMjI1NWYzXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_SX300.jpg',
+      imdbID: 'tt13833688'
+    },
+    {
+      Title: 'Top Gun: Maverick',
+      Plot: 'After more than thirty years of service as one of the Navy\'s top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_SX300.jpg',
+      imdbID: 'tt1745960'
+    },
+    {
+      Title: 'Oppenheimer',
+      Plot: 'The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg',
+      imdbID: 'tt15398776'
+    },
+    {
+      Title: 'Dune',
+      Plot: 'A noble family becomes embroiled in a war for control over the galaxy\'s most valuable asset while its heir becomes troubled by visions of a dark future.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg',
+      imdbID: 'tt1160419'
+    },
+    {
+      Title: 'Barbie',
+      Plot: 'Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNjU3N2QxNzYtMjk1NC00MTc4LTk1NTQtMmUxNTljM2I0NDA5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
+      imdbID: 'tt1517268'
+    },
+    {
+      Title: 'The Batman',
+      Plot: 'When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the city\'s hidden corruption and question his family\'s involvement.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg',
+      imdbID: 'tt1877830'
+    },
+    {
+      Title: 'Poor Things',
+      Plot: 'The incredible tale about the fantastical evolution of Bella Baxter, a young woman brought back to life by the brilliant and unorthodox scientist Dr. Godwin Baxter.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNGIyYWMzNjktNDE3MC00YWQyLWEyMmEtN2ZmNzZhZDk3NGJlXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg',
+      imdbID: 'tt14230458'
+    },
+    {
+      Title: 'Killers of the Flower Moon',
+      Plot: 'Members of the Osage tribe in the United States are murdered under mysterious circumstances in the 1920s, sparking a major F.B.I. investigation involving J. Edgar Hoover.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BZWY5ZDVjNTUtODI5Yy00MjFhLWEyM2EtYzZjM2VjZTI0MTBjXkEyXkFqcGc@._V1_SX300.jpg',
+      imdbID: 'tt5537002'
+    },
+    {
+      Title: 'Avatar: The Way of Water',
+      Plot: 'Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na\'vi race to protect their home.',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_SX300.jpg',
+      imdbID: 'tt1630029'
+    }
+  ];
+
+
+  protected readonly MatMonthView = MatMonthView
 }
