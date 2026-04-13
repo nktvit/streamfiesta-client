a270d9abcb5a74b70e6c435f58334dfe46d1cca2
nktvit <nikita.vitkovsky@icloud.com>
2026-04-13 23:54:16 +0800

    feat: vercel proxy

--- src/app/components/movie-player/movie-player.component.ts
+++ src/app/components/movie-player/movie-player.component.ts
@@ -26,7 +26,7 @@ export class MoviePlayerComponent implements OnChanges {
       return null;
     }
 
-    const baseUrl = 'https://vidsrc-embed.ru/embed';
+    const baseUrl = '/embed';
     const type = this.type() === 'tv' ? 'tv' : 'movie';
     let embedUrl = `${baseUrl}/${type}/${this.imdbId()}`;
 
