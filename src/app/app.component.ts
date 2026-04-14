502cb44b5b294c5db539f5522472347591861bfc
nktvit <nikita.vitkovsky@icloud.com>
2026-04-14 12:36:05 +0800

    feat: @vercel/speed-insights is installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { initFlowbite } from "flowbite";
 import { RouterOutlet } from '@angular/router';
 import { NavigationService } from './services/navigation.service';
 import { FooterComponent } from './components/footer/footer.component';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ export class AppComponent {
 
   ngOnInit() {
     this.nav.init();
+    injectSpeedInsights();
     try {
       initFlowbite();
     } catch (e) {
