4dc3e6a7db091d1622b7d0117566007d4a0cb5ad
nktvit <nikita.vitkovsky@icloud.com>
2026-04-22 20:23:04 +0800

    refactor: removed flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { initFlowbite } from "flowbite";
 import { RouterOutlet } from '@angular/router';
 import { NavigationService } from './services/navigation.service';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,10 +17,5 @@ export class AppComponent {
   ngOnInit() {
     this.nav.init();
     injectSpeedInsights();
-    try {
-      initFlowbite();
-    } catch (e) {
-      console.warn('Flowbite init failed:', e);
-    }
   }
 }
